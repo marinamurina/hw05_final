9a28cdd477ec675a3806a17b6028c6027a5b5b09
Марина Мурина <marina_murina@bk.ru>
2022-08-07 03:20:00 +0300

    Спринт 6 Проверка 1 В1

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 
 def page_not_found(request, exception):
-    return render(request, 'core/404.html',{
+    return render(request, 'core/404.html', {
         'path': request.path}, status=404
-)
+    )
 
 
 def csrf_failure(request, reason=''):
